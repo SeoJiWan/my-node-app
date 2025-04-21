a26093ef56a9b58d6878b7306ac205474fd977cd
SeoJiWan <wana1997@naver.com>
2025-04-21 21:48:55 +0900

    runner test7

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => res.send('Hello CI/CD! gitlab runner 등록 성공!!!!!'));
+app.get('/', (req, res) => res.send('Hello CI/CD! gitlab runner 등록 성공!!!!! 개인 서버 기반 GitLab CI/CD 구축'));
 app.listen(port, () => console.log(`Server running on port ${port}`));
