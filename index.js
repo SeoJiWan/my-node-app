a10705d24e8239ee87eb53ae8a3249690fd47092
SeoJiWan <wana1997@naver.com>
2025-04-21 21:39:08 +0900

    runner test4

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => res.send('Hello CI/CD! test2'));
+app.get('/', (req, res) => res.send('Hello CI/CD! gitlab runner 등록 성공!!!!!'));
 app.listen(port, () => console.log(`Server running on port ${port}`));
